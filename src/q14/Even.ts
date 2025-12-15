1bc5082ba83197e8cd724dfba4ed6d292cf61961
Zone <shisanupong5415@gmail.com>
2025-12-15 18:12:49 +0700

    Even

--- src/q14/Even.ts
+++ src/q14/Even.ts
@@ -3,7 +3,7 @@ const num = Number(process.argv[2]);
 if (isNaN(num) === false) {
     for (let i = 0; i <= num; i++) {
         if (i % 2 === 0) {
-            console.log(i);
+            console.log(String(i));
         }
     }
 } else {
